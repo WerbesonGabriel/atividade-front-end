28a57d1a9845eebb9e4c2a3a3883e2a813c03f6b
WerbesonGabriel <rochawerbeson@gmail.com>
2024-09-08 14:28:39 -0300

    Add files via upload

---
+++ pagina de atletas/app.js
@@ -0,0 +1,43 @@
+dados.forEach(function (atleta) {
+    console.log("Título: " + atleta.titulo);
+    console.log("Descrição: " + atleta.descricao);
+    console.log("Link: " + atleta.link);
+    console.log("-------------------");
+});
+function pesquisar() {
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value;
+  if (!campoPesquisa) {
+        section.innerHTML = "<P>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
+        return
+        }
+  campoPesquisa = campoPesquisa.toLowerCase();
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    if (
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      resultados += `
+            <div class="item-resultado">
+<h2>
+    <a href="#" target="_blank">${dado.titulo}</a>
+</h2>
+<p class="descricao-meta">${dado.descricao}</p>
+<a href=${dado.link} target="_blank">mais informações</a>
+</div>
+`;
+    }
+  }
+  if (!resultados) {
+    ("<P>nada foi encontrado</P>");
+  }
+  section.innerHTML = resultados;
+}
